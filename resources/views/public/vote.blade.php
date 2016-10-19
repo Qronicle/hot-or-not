5c85f07e6b86b4c564e730e99e1bcb1584f18a1c
Qronicle <ruud.seberechts@gmail.com>
2016-10-19 19:14:11 +0000

    Fixed Safari CSS issue

--- resources/views/public/vote.blade.php
+++ resources/views/public/vote.blade.php
@@ -16,43 +16,53 @@
 @section('footer')
     @if ($subjects)
         <script>
-            $(function(){
+            $(function () {
                 var $form = $('form.face-off');
                 var canChoose = true;
                 var html = false;
                 var isOut = false;
-                var key = $form.find('')
                 $form.find('.incoming').removeClass('incoming');
-                $form.on('click', '.subject-img', function(e){
-                    $form.css('min-height', $form.height() + 'px');
+                $form.on('click', '.subject-img', function (e) {
                     e.preventDefault();
+                    // Prevent page from jumping to the top when next images are loading
+                    $form.css('min-height', $form.height() + 'px');
+                    // Prevent images from scaling automatically on small devices, because render error in Safari
+                    //$form.find('.subject-img').each(function() {
+                        $(this).css('height', $(this).height() + 'px');
+                        //$(this).css('min-width', $(this).width() + 'px');
+                    //});
                     if (!canChoose) return;
                     isOut = false;
                     canChoose = false;
                     $.ajax({
                         url: '/vote',
                         type: 'post',
-                        data: {key: $form.find('input[name=key]').val(), subject: $(this).val()},
-                        headers: {'X-CSRF-TOKEN': $form.find('input[name=_token]').val()},
-                        success: function(d) {
+                        data: {
+                            key: $form.find('input[name=key]').val(),
+                            subject: $(this).val()
+                        },
+                        headers: {
+                            'X-CSRF-TOKEN': $form.find('input[name=_token]').val()
+                        },
+                        success: function (d) {
                             html = d;
                             startNewRound();
                         },
-                        error: function() {
-
+                        error: function () {
+                            alert('Could not load the next match. Please refresh the page');
                         }
                     });
                     $(this).closest('.subject').addClass('the-chosen-one').siblings().addClass('the-deleted-one');
-                    setTimeout(function() {
+                    setTimeout(function () {
                         isOut = true;
                         startNewRound();
                     }, 1500);
                 });
 
-                function startNewRound()
-                {
+                function startNewRound() {
+                    //@todo When html not set, display a loader or something
                     if (!html || !isOut) return;
-                    $form.fadeOut(200, function(){
+                    $form.fadeOut(200, function () {
                         $form.html(html).show().find('.incoming').removeClass('incoming');
                         canChoose = true;
                     });
